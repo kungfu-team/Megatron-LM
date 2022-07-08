61e332d7e4a86e7f65dd229f35a237c90eb1219d
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2022-07-08 13:46:18 +0000

    clean up

--- preprocess_data.sh
+++ preprocess_data.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-# python tools/preprocess_data.py \
-python tools/preprocess_data_dp_rank.py \
+python tools/preprocess_data.py \
 	--input "/data/enwiki/json/wiki.json" \
 	--output-prefix "/data/megatron-lm/bert/bert" \
 	--vocab "/data/megatron-lm/bert/bert-large-uncased-vocab.txt" \
