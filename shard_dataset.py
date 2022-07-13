4683d3fd064e684194cf052b4750b580e5e4fa55
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2022-07-13 16:00:48 +0000

    first version of shard dataset

--- shard_dataset.py
+++ shard_dataset.py
@@ -1,4 +1,4 @@
-import hashlib
+import glob
 import os
 
 import numpy as np
@@ -10,82 +10,92 @@ from megatron.data.dataset_utils import (build_train_valid_test_datasets,
 from megatron.global_vars import _build_tokenizer
 
 
-def create_train_ds(prefix: str, train_val_test_num_samples):
-    train_ds, valid_ds, test_ds = build_train_valid_test_datasets(
-        data_prefix=[prefix],
-        data_impl='infer',
-        splits_string='949,50,1',
-        train_valid_test_num_samples=train_val_test_num_samples,
-        max_seq_length=1024,
-        masked_lm_prob=0.15,
-        short_seq_prob=0.1,
-        seed=1234,
-        skip_warmup=True,
-        binary_head=True)
+def save_shard(samples: dict, dp_rank: int, out_dir):
+    if len(samples['text']) == 0:
+        return
 
-    return train_ds
+    text = np.concatenate(samples['text'])
+    types = np.concatenate(samples['types'])
+    labels = np.concatenate(samples['labels'])
+    is_random = np.concatenate(samples['is_random'])
+    loss_mask = np.concatenate(samples['loss_mask'])
+    padding_mask = np.concatenate(samples['padding_mask'])
+    truncated = np.concatenate(samples['truncated'])
+
+    shard_num = samples['shard_num']
+    print(f'DP rank {dp_rank}: save shard {shard_num}')
+    print(f'DP rank {dp_rank}: text shape {text.shape}')
+
+    rank_dir = os.path.join(out_dir, str(dp_rank))
+    os.makedirs(rank_dir, exist_ok=True)
+    path = os.path.join(rank_dir, f'samples_{shard_num:09d}.npz')
+    np.savez(path,
+             text=text,
+             types=types,
+             labels=labels,
+             is_random=is_random,
+             loss_mask=loss_mask,
+             padding_mask=padding_mask,
+             truncated=truncated)
+
+    samples['shard_num'] += 1
+
+    samples['text'] = []
+    samples['types'] = []
+    samples['labels'] = []
+    samples['is_random'] = []
+    samples['loss_mask'] = []
+    samples['padding_mask'] = []
+    samples['truncated'] = []
 
 
-def hash_head(ds, num):
-    for i, sample in enumerate(ds):
-        b = sample['text'].tobytes()
-        h = hashlib.sha256(b).hexdigest()
-        print(f'{i:02d}: {h}')
+def shard_dataset(ds_dir: str, batch_size: int, dp_size: int, out_dir: str,
+                  num_samples: int):
+    device_batch_size = batch_size // dp_size
 
-        if i >= num:
-            break
+    samples = {}
+    for dp_rank in range(dp_size):
+        samples[dp_rank] = {}
+        samples[dp_rank]['text'] = []
+        samples[dp_rank]['types'] = []
+        samples[dp_rank]['labels'] = []
+        samples[dp_rank]['is_random'] = []
+        samples[dp_rank]['loss_mask'] = []
+        samples[dp_rank]['padding_mask'] = []
+        samples[dp_rank]['truncated'] = []
+        samples[dp_rank]['shard_num'] = 0
 
+    sample_num = 0
+    sample_files = glob.glob(os.path.join(ds_dir, 'samples_*'))
+    for sample_file in sample_files:
+        sample_shard = np.load(sample_file)
+        for i in range(sample_shard['text'].shape[0]):
+            batch_idx = sample_num % batch_size
+            dp_rank = batch_idx // device_batch_size
+            samples[dp_rank]['text'].append(sample_shard['text'][i])
+            samples[dp_rank]['types'].append(sample_shard['types'][i])
+            samples[dp_rank]['labels'].append(sample_shard['labels'][i])
+            samples[dp_rank]['is_random'].append(sample_shard['is_random'][i])
+            samples[dp_rank]['loss_mask'].append(sample_shard['loss_mask'][i])
+            samples[dp_rank]['padding_mask'].append(
+                sample_shard['padding_mask'][i])
+            samples[dp_rank]['truncated'].append(sample_shard['truncated'][i])
 
-def load_samples(ds, num):
-    for i, sample in enumerate(ds):
-        if i >= num:
-            break
+            if len(samples[dp_rank]['text']) == num_samples:
+                save_shard(samples[dp_rank], dp_rank, out_dir)
 
-
-def save_batches(ds, num_samples=16384):
-    samples = []
-    shard_num = 0
-    for i, sample in enumerate(ds):
-        if i > 0 and i % num_samples == 0:
-            # save
-            text = np.concatenate([[x['text']] for x in samples])
-            types = np.concatenate([[x['types']] for x in samples])
-            labels = np.concatenate([[x['labels']] for x in samples])
-            is_random = np.concatenate([[x['is_random']] for x in samples])
-            loss_mask = np.concatenate([[x['loss_mask']] for x in samples])
-            padding_mask = np.concatenate([[x['padding_mask']]
-                                           for x in samples])
-            truncated = np.concatenate([[x['truncated']] for x in samples])
-
-            print(f'save shard {shard_num}')
-
-            path = '/data/megatron-lm/bert/large_1024'
-            path = os.path.join(path, f'samples_{shard_num:09d}.npz')
-            np.savez(path,
-                     text=text,
-                     types=types,
-                     labels=labels,
-                     is_random=is_random,
-                     loss_mask=loss_mask,
-                     padding_mask=padding_mask,
-                     truncated=truncated)
-
-            shard_num = shard_num + 1
-            samples = []
-
-        samples.append(sample)
+    for dp_rank in range(dp_size):
+        save_shard(samples[dp_rank], dp_rank, out_dir)
 
 
 def main():
     batch_size = 32
-    dp_degree = 2
-    device_batch_size = batch_size // dp_degree
-    eval_iters = 10
-    train_iters = 4000
-
-    full_ds_dir = '/data/megatron-lm/bert/bert_text_sentence'
-    full_train_ds = create_train_ds(prefix, train_val_test_num_samples)
-    save_batches(full_train_ds)
+    dp_size = 2
+    seq_len = 512
+    num_samples = 16384
+    full_ds_dir = f'/data/megatron-lm/bert/large_{seq_len}'
+    out_dir = f'/data/megatron-lm/bert/shard{dp_size}_{seq_len}'
+    shard_dataset(full_ds_dir, batch_size, dp_size, out_dir, num_samples)
 
 
 if __name__ == '__main__':
