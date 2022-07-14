61e9ace278940b4819afd5495ad02d9713f17758
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2022-07-14 08:21:10 +0000

    use indexing instead of appending

--- shard_dataset.py
+++ shard_dataset.py
@@ -1,5 +1,6 @@
 import glob
 import os
+from typing import Dict
 
 import numpy as np
 import torch
@@ -10,92 +11,83 @@ from megatron.data.dataset_utils import (build_train_valid_test_datasets,
 from megatron.global_vars import _build_tokenizer
 
 
-def save_shard(samples: dict, dp_rank: int, out_dir):
-    if len(samples['text']) == 0:
+def save_shard(samples: dict, dp_rank: int, out_dir: str,
+               samples_per_shard: int, keys: list):
+    total_num_samples = samples['text'].shape[0]
+    if total_num_samples == 0:
         return
 
-    text = np.concatenate(samples['text'])
-    types = np.concatenate(samples['types'])
-    labels = np.concatenate(samples['labels'])
-    is_random = np.concatenate(samples['is_random'])
-    loss_mask = np.concatenate(samples['loss_mask'])
-    padding_mask = np.concatenate(samples['padding_mask'])
-    truncated = np.concatenate(samples['truncated'])
+    num_shards = total_num_samples // samples_per_shard
+    for shard_idx in range(num_shards):
+        start_idx = shard_idx * samples_per_shard
+        end_idx = start_idx + samples_per_shard
 
-    shard_num = samples['shard_num']
-    print(f'DP rank {dp_rank}: save shard {shard_num}')
-    print(f'DP rank {dp_rank}: text shape {text.shape}')
+        new_shard: Dict[str, np.ndarray] = {}
+        for key in keys:
+            new_shard[key] = samples[key][start_idx:end_idx]
 
-    rank_dir = os.path.join(out_dir, str(dp_rank))
-    os.makedirs(rank_dir, exist_ok=True)
-    path = os.path.join(rank_dir, f'samples_{shard_num:09d}.npz')
-    np.savez(path,
-             text=text,
-             types=types,
-             labels=labels,
-             is_random=is_random,
-             loss_mask=loss_mask,
-             padding_mask=padding_mask,
-             truncated=truncated)
+        rank_dir = os.path.join(out_dir, str(dp_rank))
+        os.makedirs(rank_dir, exist_ok=True)
+        path = os.path.join(rank_dir, f'samples_{shard_idx:09d}.npz')
+        np.savez(path, **new_shard)
 
-    samples['shard_num'] += 1
+        print(f'finished shard {shard_idx}')
 
-    samples['text'] = []
-    samples['types'] = []
-    samples['labels'] = []
-    samples['is_random'] = []
-    samples['loss_mask'] = []
-    samples['padding_mask'] = []
-    samples['truncated'] = []
+
+def concat(samples, key, tail):
+    if key in samples:
+        return np.concatenate((samples[key], tail))
+    return tail
 
 
 def shard_dataset(ds_dir: str, batch_size: int, dp_size: int, out_dir: str,
-                  num_samples: int):
+                  samples_per_shard: int, keys: list):
     device_batch_size = batch_size // dp_size
 
-    samples = {}
+    samples: Dict[int, dict] = {}
     for dp_rank in range(dp_size):
         samples[dp_rank] = {}
-        samples[dp_rank]['text'] = []
-        samples[dp_rank]['types'] = []
-        samples[dp_rank]['labels'] = []
-        samples[dp_rank]['is_random'] = []
-        samples[dp_rank]['loss_mask'] = []
-        samples[dp_rank]['padding_mask'] = []
-        samples[dp_rank]['truncated'] = []
-        samples[dp_rank]['shard_num'] = 0
 
-    sample_num = 0
     sample_files = glob.glob(os.path.join(ds_dir, 'samples_*'))
-    for sample_file in sample_files:
-        sample_shard = np.load(sample_file)
-        for i in range(sample_shard['text'].shape[0]):
-            batch_idx = sample_num % batch_size
-            dp_rank = batch_idx // device_batch_size
-            samples[dp_rank]['text'].append(sample_shard['text'][i])
-            samples[dp_rank]['types'].append(sample_shard['types'][i])
-            samples[dp_rank]['labels'].append(sample_shard['labels'][i])
-            samples[dp_rank]['is_random'].append(sample_shard['is_random'][i])
-            samples[dp_rank]['loss_mask'].append(sample_shard['loss_mask'][i])
-            samples[dp_rank]['padding_mask'].append(
-                sample_shard['padding_mask'][i])
-            samples[dp_rank]['truncated'].append(sample_shard['truncated'][i])
 
-            if len(samples[dp_rank]['text']) == num_samples:
-                save_shard(samples[dp_rank], dp_rank, out_dir)
+    for i, sample_file in enumerate(sample_files):
+        sample_shard = np.load(sample_file)
+        shard_num_samples = sample_shard['text'].shape[0]
+
+        for dp_rank in range(dp_size):
+            # create indices
+            dp_indices = []
+            num_batches = shard_num_samples // batch_size
+            for batch in range(num_batches):
+                start_idx = batch * batch_size
+                start_idx = start_idx + dp_rank * device_batch_size
+                end_idx = start_idx + device_batch_size
+                indices = list(range(start_idx, end_idx))
+                dp_indices.extend(indices)
+
+            for key in keys:
+                samples[dp_rank][key] = concat(samples[dp_rank], key,
+                                               sample_shard[key][dp_indices])
+
+        print(f'finished file {i}')
 
     for dp_rank in range(dp_size):
-        save_shard(samples[dp_rank], dp_rank, out_dir)
+        save_shard(samples[dp_rank], dp_rank, out_dir, samples_per_shard, keys)
 
 
 def main():
     batch_size = 32
     dp_size = 2
     seq_len = 512
-    num_samples = 16384
+    samples_per_shard = 16384
     full_ds_dir = f'/data/megatron-lm/bert/large_{seq_len}'
-    out_dir = f'/data/megatron-lm/bert/shard{dp_size}_{seq_len}'
-    shard_dataset(full_ds_dir, batch_size, dp_size, out_dir, num_samples)
+    out_dir = f'/data/megatron-lm/bert/shard{dp_size}_seq{seq_len}'
+    keys = [
+        'text', 'types', 'labels', 'is_random', 'loss_mask', 'padding_mask',
+        'truncated'
+    ]
+    shard_dataset(full_ds_dir, batch_size, dp_size, out_dir, samples_per_shard,
+                  keys)
 
 
 if __name__ == '__main__':
