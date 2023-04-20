afa0170efbc30640ad19d2df44154fa8b46749db
Marcel Wagenlaender <marcel.wagenlander19@imperial.ac.uk>
2023-04-20 21:58:30 +0000

    rename

--- run_compile_kernels.sh
+++ run_compile_kernels.sh
@@ -3,5 +3,8 @@
 docker run \
     --gpus all \
     --name tenplex-compile \
-    -t kungfu.azurecr.io/mw-megatron-lm-no-scheduler:latest \
+    -t kungfu.azurecr.io/mw-megatron-lm:latest \
     python compile_kernels.py
+
+docker commit tenplex-compile kungfu.azurecr.io/mw-megatron-lm-kernel:latest
+docker push kungfu.azurecr.io/mw-megatron-lm-kernel:latest
