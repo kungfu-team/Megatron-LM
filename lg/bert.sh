56019902b0c7d384afa69e5fcf910ea0e4dacb18
lg <lg4869@outlook.com>
2024-04-28 00:53:58 +0000

    u

--- lg/bert.sh
+++ lg/bert.sh
@@ -10,6 +10,25 @@ export CUDA_DEVICE_MAX_CONNECTIONS=1
 # VOCAB_FILE=<Specify path to file>/bert-vocab.txt
 # DATA_PATH=<Specify path and file prefix>_text_sentence
 
+bert_flags() {
+    echo --num-layers 24
+    echo --hidden-size 1024
+    echo --num-attention-heads 16
+    echo --seq-length 512
+    echo --max-position-embeddings 512
+    echo --micro-batch-size 4
+    echo --global-batch-size 8
+    echo --lr 0.0001
+    echo --train-iters 2000000
+    echo --lr-decay-iters 990000
+    echo --lr-decay-style linear
+    echo --min-lr 0.00001
+    echo --weight-decay 1e-2
+    echo --lr-warmup-fraction .01
+    echo --clip-grad 1.0
+    echo --fp16
+}
+
 data_flags() {
     echo --data-path $DATA_PATH
     echo --vocab-file $VOCAB_FILE
@@ -18,14 +37,14 @@ data_flags() {
 }
 
 output_flags() {
-    echo --log-interval 100
+    echo --log-interval 10
     echo --save-interval 10000
     echo --eval-interval 1000
     echo --eval-iters 10
 }
 
 flags() {
-    ./bert-args.sh
+    bert_flags
     data_flags
     output_flags
     --save $CHECKPOINT_PATH
