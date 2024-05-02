0491b79c3b97a824582543b311f433ea100cc584
lg <lg4869@outlook.com>
2024-05-02 15:50:40 +0000

    u

--- lg/gen-torch-requirements.c
+++ lg/gen-torch-requirements.c
@@ -2,17 +2,21 @@
 
 #define PRN(fmt, ...) fprintf(stdout, fmt "\n", __VA_ARGS__)
 
-void gen_torch_requirements(const char *cuv)
+#define FPRN(fp, fmt, ...) fprintf(fp, fmt "\n", __VA_ARGS__)
+
+void gen_torch_requirements(FILE *fp, const char *cuv)
 {
-    PRN("#!/usr/bin/env -S sh -c 'python3 -m pip install -f %s -r $0'",
-        "https://download.pytorch.org/whl/torch_stable.html");
-    PRN("torch==%s+%s", "2.1.1", cuv);
-    PRN("torchaudio==%s+%s", "2.1.1", cuv);
-    PRN("torchvision==%s+%s", "0.16.1", cuv);
+    FPRN(fp, "#!/usr/bin/env -S sh -c 'python3 -m pip install -f %s -r $0'",
+         "https://download.pytorch.org/whl/torch_stable.html");
+    FPRN(fp, "torch==%s+%s", "2.1.1", cuv);
+    FPRN(fp, "torchaudio==%s+%s", "2.1.1", cuv);
+    FPRN(fp, "torchvision==%s+%s", "0.16.1", cuv);
 }
 
 int main()
 {
-    gen_torch_requirements("cu118");
+    FILE *fp = fopen("requirements.torch.txt", "w");
+    gen_torch_requirements(fp, "cu118");
+    fclose(fp);
     return 0;
 }
