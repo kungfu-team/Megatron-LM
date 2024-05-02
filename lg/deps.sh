99761ce07a8969a8a60015cf3af3241d97b48c99
lg <lg4869@outlook.com>
2024-05-02 14:00:46 +0000

    u

--- lg/deps.sh
+++ lg/deps.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 set -e
+
 cd $(dirname $0)
 
 . ./_x
 
-x ./requirements.txt
+# NVIDIA-SMI 550.54.14
+# Driver Version: 550.54.14
+# CUDA Version: 12.4
+
 x ./requirements.torch.txt
+x ./requirements.txt
 x ./check.py
 
 echo "done $0"
