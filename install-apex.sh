3de433418ba56f270502601d4202d3e5daa53f3e
lg <lg4869@outlook.com>
2024-05-02 13:46:00 +0000

    u

---
+++ install-apex.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+
+# python3 -m pip uninstall -y apex
+
+if [ ! -d apex ]; then
+    git clone https://github.com/NVIDIA/apex -b 23.06
+fi
+
+cd apex
+
+# python3 -m pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --config-settings "--build-option=--cpp_ext" --config-settings "--build-option=--cuda_ext" ./
+python3 -m pip install -v --disable-pip-version-check --no-cache-dir --no-build-isolation --global-option="--cpp_ext" --global-option="--cuda_ext" ./
+
+#   RuntimeError: Cuda extensions are being compiled with a version of Cuda that does not match the version used to compile Pytorch binaries.  Pytorch binaries were compiled with Cuda 11.8.
+#   In some cases, a minor-version mismatch will not cause later errors:  https://github.com/NVIDIA/apex/pull/323#discussion_r287021798.  You can try commenting out this check (at your own risk)
