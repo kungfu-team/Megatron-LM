fbb427a7537fc13bdb9a8fb27e090d6f8b2a30d6
lg <lg4869@outlook.com>
2024-05-02 14:18:28 +0000

    u

---
+++ lg/check-cuda-rt.c
@@ -0,0 +1,25 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define CHECK(e)                                                               \
+    if (e != cudaSuccess) {                                                    \
+        exit(1);                                                               \
+    }
+
+void show_cuda_rt()
+{
+    int runtimeVersion;
+    // https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART____VERSION.html#group__CUDART____VERSION
+    cudaError_t ret = cudaRuntimeGetVersion(&runtimeVersion);
+    CHECK(ret);
+    printf("%d\n", runtimeVersion);
+}
+
+int main()
+{
+    // https://docs.nvidia.com/cuda/cuda-runtime-api/group__CUDART__DEVICE.html#group__CUDART__DEVICE
+    cudaInitDevice(0, 0, 0);
+    show_cuda_rt();
+    return 0;
+}
