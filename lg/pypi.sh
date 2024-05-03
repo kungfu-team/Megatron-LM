d980fa1121079a6fbc6c065c93eadd3cb3adcac9
lg <lg4869@outlook.com>
2024-05-03 10:00:49 +0000

    u

--- lg/pypi.sh
+++ lg/pypi.sh
@@ -3,6 +3,9 @@ set -e
 
 cd $(dirname $0)
 
-ie https://download.pytorch.org/whl/torch_stable.html >torch.txt
+# url=https://download.pytorch.org/whl/cu118
+url=https://download.pytorch.org/whl/torch_stable.html
+
+ie $url >torch.txt
 
 echo "done $0"
