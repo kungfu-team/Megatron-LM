5f00c19633a65ed696b7cb4af96c55adf8f29a4b
lg <lg4869@outlook.com>
2024-05-09 16:15:05 +0000

    u

--- lg/bert_model.py
+++ lg/bert_model.py
@@ -1,14 +1,17 @@
-import torch
-from megatron import get_args, mpu
+from megatron import get_args, initialize
 from megatron.core.enums import ModelType
 from megatron.initialize import initialize_megatron
 from megatron.model import BertModel
-from megatron.training import setup_model_and_optimizer
+
+
+def noop(*_args, **_kvargs):
+    pass
 
 
 def build_model(pre_process=True, post_process=True):
 
     args = get_args()
+    args.model_type = ModelType.encoder_or_decoder
     num_tokentypes = 2 if args.bert_binary_head else 0
     model = BertModel(
         num_tokentypes=num_tokentypes,
@@ -22,18 +25,13 @@ def build_model(pre_process=True, post_process=True):
 
 
 def main():
+    initialize._compile_dependencies = noop
     initialize_megatron(
         extra_args_provider=None,
         args_defaults={
             'tokenizer_type': 'BertWordPieceLowerCase',
         },
     )
-    # model_type = ModelType.encoder_or_decoder
-    # model, optimizer, opt_param_scheduler = setup_model_and_optimizer(
-    #     model_provider, model_type)
-
-    # torch.distributed.init_process_group('nccl')
-    # mpu.initialize_model_parallel(1, 1, 1, 0)
 
     m = build_model()
     print(m)
