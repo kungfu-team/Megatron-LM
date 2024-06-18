5e54b17211cf3efe16d70e6c2207e8449cf30d05
lg <lg4869@outlook.com>
2024-06-18 15:46:44 +0000

    update scripts

--- hash_samples.py
+++ hash_samples.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env torchrun
 import hashlib
 
 import numpy as np
--- hash_samples.sh
+++ hash_samples.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
+idx=/data/megatron-lm/gpt-2/enwiki/npzs_seq1024_new/indices.txt
+# idx=/data/megatron-lm/gpt-2/enwiki/npzs_seq1024/indices.txt
+
 mlfs mount \
-    -index-url "/data/megatron-lm/gpt-2/enwiki/npzs_seq1024_new/indices.txt" \
+    -index-url $idx \
     -job tenplex-samples \
     -global-batch-size 128 \
     -no-shuffle
 
-docker run \
-    --rm \
-    -it \
-    -v /mnt/mlfs:/data/mlfs \
-    -v ./out:/data/out \
-    -v /data/megatron-lm:/data/dataset \
-    kungfu.azurecr.io/mw-megatron-lm-23.06-debug:latest \
-    /bin/bash
+# docker run \
+#     --rm \
+#     -it \
+#     -v /mnt/mlfs:/data/mlfs \
+#     -v ./out:/data/out \
+#     -v /data/megatron-lm:/data/dataset \
+#     kungfu.azurecr.io/mw-megatron-lm-23.06-debug:latest \
+#     /bin/bash
